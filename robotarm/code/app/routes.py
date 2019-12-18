0b95d1c9661dc1c592e2867135b30dce2bda74e9
idjaeha <jaehanim1@gmail.com>
2019-12-18 13:30:57 +0900

    fix submit error

--- robotarm/code/app/routes.py
+++ robotarm/code/app/routes.py
@@ -2,15 +2,14 @@
 from flask import Flask, render_template, url_for, redirect, request
 from app import app
 
-@app.route('/<value>' , methods=["GET"])
-def index(value=3):
-    return render_template('index.html', value=value)
-
-@app.route('/', methods=["POST"])
-def indexPost():
-    value = request.args.get("value")
-    return render_template('index.html', vaule=value)
-
+@app.route('/' , methods=["GET", "POST"])
+def index(value = 3):
+    if request.method == "GET":
+        return render_template('index.html', value=value)
+    else:
+        value = int(request.form["slider"])
+        return render_template('index.html', value=value)
+    
 @app.route('/profile/<username>/')
 def get_profile(username):
     return url_for("test_cookies", myname=username)
--- robotarm/code/app/static/javascript/slidebar.js
+++ robotarm/code/app/static/javascript/slidebar.js
@@ -1,7 +1,5 @@
 function sendValue() {
-    var value = document.getElementById('slider_range');
     var slidebar_view = document.getElementById('slider_value_view');
-    slidebar_view.value = value;
     var form = document.getElementById('robotarmdata');
     form.submit();
 }
